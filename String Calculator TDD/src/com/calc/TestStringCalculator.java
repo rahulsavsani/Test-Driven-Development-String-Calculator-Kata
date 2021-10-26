a2e82de595eb01776d2f6496ebae3ad487aa8768
Rahul Savsani <rahulsavsani@gmail.com>
2021-10-26 19:05:10 +0530

    Refactor Phase

--- String Calculator TDD/src/com/calc/TestStringCalculator.java
+++ String Calculator TDD/src/com/calc/TestStringCalculator.java
@@ -2,13 +2,35 @@ package com.calc;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class TestStringCalculator {
 	
 	private static StringCalculator sc;
+	private String numbers;
+	private int expectedResult;
 	
+	public TestStringCalculator(String numbers, int expectedResult) {
+		super();
+		this.numbers = numbers;
+		this.expectedResult = expectedResult;
+	}
+	
+	@Parameters
+	public static Iterable<Object[]> inputs() {
+		return Arrays.asList(new Object[][] {
+			{"1",1},
+			{"2",2}
+		});
+	}
+
 	@BeforeClass
 	public static void initialize() {
 		
@@ -24,12 +46,7 @@ public class TestStringCalculator {
 	@Test
 	public void addSingleNumReturnsNum() {
 		
-		assertEquals(1, sc.add("1"));
+		assertEquals(expectedResult, sc.add(numbers));
 	}
 	
-	@Test
-	public void addSingleNum2ReturnsNum() {
-		
-		assertEquals(2, sc.add("2"));
-	}
 }
