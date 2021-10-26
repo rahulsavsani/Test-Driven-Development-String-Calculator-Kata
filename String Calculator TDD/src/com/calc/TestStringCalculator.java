bd77522e216a509155de17dfd51b39f3f98128c1
Rahul Savsani <rahulsavsani@gmail.com>
2021-10-26 18:26:32 +0530

    Feature 1.2 (Single Num) : Refactor Phase

--- String Calculator TDD/src/com/calc/TestStringCalculator.java
+++ String Calculator TDD/src/com/calc/TestStringCalculator.java
@@ -2,22 +2,27 @@ package com.calc;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestStringCalculator {
+	
+	private static StringCalculator sc;
+	
+	@BeforeClass
+	public static void initialize() {
+		
+		sc = new StringCalculator();
+	}
 
 	@Test
 	public void addEmptyStringReturnsZero() {
-		
-		StringCalculator sc = new StringCalculator();
 		
 		assertEquals(0, sc.add(""));
 	}
 	
 	@Test
 	public void addSingleNumReturnsNum() {
-		
-		StringCalculator sc = new StringCalculator();
 		
 		assertEquals(1, sc.add("1"));
 	}
